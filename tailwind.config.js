daf886a7dd07875ee5f14bddc78626b57cb35a4e
nopkrup <nopkrup@gmail.com>
2025-06-11 11:45:08 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
