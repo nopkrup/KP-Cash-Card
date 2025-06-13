46c35ccf8ebc439af41c37f67c63fc961b68cfe9
nopkrup <nopkrup@gmail.com>
2025-06-14 01:21:20 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ export default function App() {
                   <p>มูลค่า Cash Card รวม: {result.totalValue2.toLocaleString()} บาท</p>
                   <p className="mt-2 font-semibold text-blue-800">💳 ชำระเงินค่า Cash Card: {result.totalPaid2.toLocaleString()} บาท</p>
                   <p className="font-bold text-red-600 text-xl mt-2">💰 รวมลูกค้าต้องจ่ายทั้งหมด: {result.totalToPay2.toLocaleString()} บาท</p>
-                  <p className="text-green-600 font-bold mt-2">ส่วนลดที่ได้รับ คิดจากจ่ายจริง/ราคาสินค้า: {result.discountAmount2.toLocaleString()} บาท ({result.discountPercent2}%) </p>
+                  <p className="text-green-600 font-bold mt-2">ส่วนลดที่ได้รับ : {result.discountAmount2.toLocaleString()} บาท ({result.discountPercent2}%)</p>
                   <p className="text-green-600 font-bold mt-2">ℹ️แท้จริงลูกค้าได้รับ 30%  แต่มีเงินเหลือ *ถ้าส่วนลดติดลบ เกิดจากเงินเพิ่มไม่ถูกใช้ แนะนำตัวเลือก🅰️</p>
                   <p className="text-blue-700 font-semibold">📌 Cash Card คงเหลือ: {result.remainingCashCardValue.toLocaleString()} บาท</p>
                 </>
